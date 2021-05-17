093cb1304b272bfbf42ca9af17984d86a3e32954
COAZY <67540016+coazyy@users.noreply.github.com>
2021-05-17 16:43:47 +0200

    Add files via upload

---
+++ VPN-Application-CSharp/Main.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace VPN_Application_CSharp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.siticoneRoundedComboBox1 = new Siticone.UI.WinForms.SiticoneRoundedComboBox();
+            this.siticoneRoundedButton1 = new Siticone.UI.WinForms.SiticoneRoundedButton();
+            this.siticoneRoundedButton2 = new Siticone.UI.WinForms.SiticoneRoundedButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticoneElipse1 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+            this.siticoneTransparentDrag1 = new Siticone.UI.WinForms.SiticoneTransparentDrag();
+            this.SuspendLayout();
+            // 
+            // siticoneRoundedComboBox1
+            // 
+            this.siticoneRoundedComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneRoundedComboBox1.BorderThickness = 0;
+            this.siticoneRoundedComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.siticoneRoundedComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.siticoneRoundedComboBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.siticoneRoundedComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.siticoneRoundedComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.siticoneRoundedComboBox1.FormattingEnabled = true;
+            this.siticoneRoundedComboBox1.HoveredState.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.ItemHeight = 30;
+            this.siticoneRoundedComboBox1.Items.AddRange(new object[] {
+            "Server 1",
+            "Server 2"});
+            this.siticoneRoundedComboBox1.ItemsAppearance.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.Location = new System.Drawing.Point(129, 108);
+            this.siticoneRoundedComboBox1.Name = "siticoneRoundedComboBox1";
+            this.siticoneRoundedComboBox1.ShadowDecoration.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.Size = new System.Drawing.Size(204, 36);
+            this.siticoneRoundedComboBox1.TabIndex = 2;
+            // 
+            // siticoneRoundedButton1
+            // 
+            this.siticoneRoundedButton1.CheckedState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.CustomImages.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneRoundedButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedButton1.HoveredState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Location = new System.Drawing.Point(129, 151);
+            this.siticoneRoundedButton1.Name = "siticoneRoundedButton1";
+            this.siticoneRoundedButton1.ShadowDecoration.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Size = new System.Drawing.Size(204, 37);
+            this.siticoneRoundedButton1.TabIndex = 3;
+            this.siticoneRoundedButton1.Text = "Connect";
+            this.siticoneRoundedButton1.Click += new System.EventHandler(this.siticoneRoundedButton1_Click);
+            // 
+            // siticoneRoundedButton2
+            // 
+            this.siticoneRoundedButton2.CheckedState.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.CustomImages.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneRoundedButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedButton2.HoveredState.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.Location = new System.Drawing.Point(129, 194);
+            this.siticoneRoundedButton2.Name = "siticoneRoundedButton2";
+            this.siticoneRoundedButton2.ShadowDecoration.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.Size = new System.Drawing.Size(204, 37);
+            this.siticoneRoundedButton2.TabIndex = 4;
+            this.siticoneRoundedButton2.Text = "Disconnect";
+            this.siticoneRoundedButton2.Click += new System.EventHandler(this.siticoneRoundedButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(122, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "VPN Application";
+            // 
+            // siticoneElipse1
+            // 
+            this.siticoneElipse1.TargetControl = this;
+            // 
+            // siticoneTransparentDrag1
+            // 
+            this.siticoneTransparentDrag1.DragEndTransparencyValue = 1D;
+            this.siticoneTransparentDrag1.DragStartTransparencyValue = 0.9D;
+            this.siticoneTransparentDrag1.TargetDragControl = this;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(470, 251);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.siticoneRoundedButton2);
+            this.Controls.Add(this.siticoneRoundedButton1);
+            this.Controls.Add(this.siticoneRoundedComboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VPN App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneRoundedComboBox siticoneRoundedComboBox1;
+        private Siticone.UI.WinForms.SiticoneRoundedButton siticoneRoundedButton1;
+        private Siticone.UI.WinForms.SiticoneRoundedButton siticoneRoundedButton2;
+        private System.Windows.Forms.Label label1;
+        private Siticone.UI.WinForms.SiticoneElipse siticoneElipse1;
+        private Siticone.UI.WinForms.SiticoneTransparentDrag siticoneTransparentDrag1;
+    }
+}
+
---
+++ VPN-Application-CSharp/Main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPN_Application_CSharp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            siticoneRoundedButton2.Enabled = false;
+        }
+
+        private void siticoneRoundedButton1_Click(object sender, EventArgs e)
+        {
+            if (siticoneRoundedComboBox1.Text == "")
+            {
+                MessageBox.Show("Please select a Server!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (siticoneRoundedComboBox1.Text == "Server 1")
+            {
+
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = @"C:\Program Files\OpenVPN\bin\openvpn.exe";
+                startInfo.Arguments = @"--config CONFIGNAME";
+                startInfo.Verb = "runas";
+                process.StartInfo = startInfo;
+                process.Start();
+                siticoneRoundedButton2.Enabled = true;
+                siticoneRoundedButton1.Enabled = false;
+                //End of connection
+            }
+            if (siticoneRoundedComboBox1.Text == "Server 2")
+            {
+
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = @"C:\Program Files\OpenVPN\bin\openvpn.exe";
+                startInfo.Arguments = @"--config CONFIGNAME2";
+                startInfo.Verb = "runas";
+                process.StartInfo = startInfo;
+                process.Start();
+                siticoneRoundedButton2.Enabled = true;
+                siticoneRoundedButton1.Enabled = false;
+                //End of connection
+            }
+        }
+
+        private void siticoneRoundedButton2_Click(object sender, EventArgs e)
+        {
+            siticoneRoundedButton1.Enabled = true;
+            siticoneRoundedButton2.Enabled = false;
+        }
+    }
+}
